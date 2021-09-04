a3cf798c58ecb509a4ab629206be63230a570682
Yuli2203 <90106859+Yuli2203@users.noreply.github.com>
2021-09-05 00:11:18 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,5 @@
+<?
+//PERHATIKAN CARA ADMIN MENGISI DATA
+
+$cookie = "_ga=GA1.1.413591598.1630762990;bitmedia_fid=eyJmaWQiOiI5NjEwMDNlYzYwNTc3NjI3YzkxNmFlYjlkNGY3YjllMSIsImZpZG5vdWEiOiJlYTkwODQwYTNjNDljNzBmZjY3OWMzZWI3ZDQxYWJmYiJ9;dom3ic8zudi28v8lr6fgphwffqoz0j6c=81647505-97f3-4197-9350-dfac9a06f334%3A2%3A1;sb_main_872de0b8471097e965e69eb00a5136b3=1;acceptCookies=true;sb_count_872de0b8471097e965e69eb00a5136b3=4;remember_me=c49563ebd70eb19e9e4dbcede4e41111c32a8863ef5396794ce3e2328f0ea2de;AUTH_SYSTEM=143ab08ae0971f687a87ac48770a5e8e;_ga_80GDFY5XTF=GS1.1.1630762989.1.1.1630765351.0;_data_cpc=1-2_15-2_132-2_188-1";
+$user_agent = "Mozilla/5.0 (Linux; Android 6.0.1; vivo 1606) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Mobile Safari/537.36";
